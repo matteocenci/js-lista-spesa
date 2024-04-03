9396516cd809f6dc6268c457f066a90d579dc21b
matteocenci <matteocenci01@gmail.com>
2024-04-03 16:33:25 +0200

    feat: while cycle done and list is created in html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 const shoppingList = [];
+let listContinue = true;
 
+//creo lista che mi continua a chiedere di inserire un prodotto
+while ( listContinue) { 
+    let listElem = prompt ("Inserisci nella lista un prodotto:")
+    console.log(listElem);
+    
+    shoppingList.push(listElem);
+    
+    if (listElem.toLowerCase() === "fine") {
+        break;
+    }
+    
+}
+
+let listElem = document.querySelector(".shopping-list");
+
+// quando finisco la lista 
+for (i = 0; i < shoppingList.length; i++) {
+    let listElemLi = document.createElement("li");
+    listElemLi.textContent = shoppingList[i];
+    listElem.appendChild(listElemLi);
+}
 
